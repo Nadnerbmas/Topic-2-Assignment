7c9f30a9a5056ca78561304e7b39a73063f11c32
363435819 <363435819@2260I-L29-023>
2026-02-04 12:58:10 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Topic_2_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Brendan McCausland
+            Console.WriteLine("I will now count my chickens:");
+            Console.WriteLine("Hens " + (10 + 5 * 2));
+            Console.WriteLine("Roosters " + (25 + 30 / 6));
+        }
+    }
+}
