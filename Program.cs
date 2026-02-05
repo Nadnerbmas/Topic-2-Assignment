06f88de9aaafface4b6beb740ff25733c9c30677
363435819 <363435819@2260I-L29-023>
2026-02-05 13:08:41 -0500

    not done

--- Program.cs
+++ Program.cs
@@ -1,13 +1,34 @@
-﻿namespace Topic_2_Assignment
+﻿using System.Globalization;
+
+namespace Topic_2_Assignment
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             //Brendan McCausland
-            Console.WriteLine("I will now count my chickens:");
-            Console.WriteLine("Hens " + (10 + 5 * 2));
-            Console.WriteLine("Roosters " + (25 + 30 / 6));
+            Console.Write("I was born in the year ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(2026 - 17);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
+
+            Console.Write("The average of the numbers; 14, 15, and 16 is... ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine((14 + 15 + 16) / 3);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
+
+            Console.Write("The average of the numbers; 14, 15, and 16 using floationg point values is... ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine((14.0 + 15.0 + 16.0 + 17.0) / 4);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
+
+            //Solve the circle question (5)
+
+            Console.WriteLine("");
+
         }
     }
 }
